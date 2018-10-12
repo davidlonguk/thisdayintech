852cf51e99206627294bf8238f9b74bc331e9943
David Long <david@firetechcamp.com>
2018-10-12 14:41:56 +0100

    Sample Model

---
+++ js/tdit-model-dl.js
@@ -0,0 +1,13 @@
+/* TDIT Model */
+var tditModel = {};
+
+tditModel.Data = [
+	{	id: "1210", title: "iPhone Launch", EventDate: "2007-10-12", EventDay: "10-12", summary: "Steve Jobs announces the first iPhone at Apple Conference in Silicon Valley",  image: "images/10-12-jobs.jpg", relatedItems: ["Apple", "Steve Job"]	},
+	{	id: "1310", title: "First Web Browser ", EventDate: "1990-10-13", EventDay: "10-13", summary: "Tim Berners-Lee invented the first web browser to display and edit the World Wide Web",  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Sir_Tim_Berners-Lee_%28cropped%29.jpg/330px-Sir_Tim_Berners-Lee_%28cropped%29.jpg", relatedItems: ["Tim Berners-Lee"]	},
+];
+
+tditModel.tags = [
+		{ name: "Apple", url: "http://www.apple.com" },
+		{ name: "Steve Jobs", url: "https://en.wikipedia.org/wiki/Steve_Jobs" },
+		{ name: "Tim Berners-Lee", url: "https://en.wikipedia.org/wiki/Tim_Berners-Lee#Career" }
+		]
