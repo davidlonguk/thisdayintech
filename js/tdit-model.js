6a950f5e9f31cb9884cd7f2b454296a179d54068
Khushbu <khushbu@firetechcamp.com>
2018-10-12 19:44:27 +0530

    model

--- js/tdit-model.js
+++ js/tdit-model.js
@@ -1,1 +1,19 @@
 /* TDIT Model */
+
+class TodayDay {
+	
+	constructor (evetData) {
+		
+	this.title = title;
+    this.summary = summary;
+    this.edate = edate;
+    this.eimage = eimage;
+    this.author = _getAuthor(evetData.author);
+	}
+	
+	_getAuthor(author) {
+    let author = "Steve";
+    return author;
+  }
+
+}
