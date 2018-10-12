28716a317835cdc4e164b0a6aec235883f6bf82f
Khushbu <khushbu@firetechcamp.com>
2018-10-12 20:38:15 +0530

    view

--- js/tdit-view.js
+++ js/tdit-view.js
@@ -1,1 +1,14 @@
 /* TDIT View */
+
+var EventView = function EventView(element) {
+  this.element = element;
+};
+
+
+EventView.prototype.render = function render(viewEvent) {
+  this.element.innerHTML = '<div class="main-div"><div class="left-panel"><h2>'+viewEvent.eventDate+'</h2><img src="'+ viewEvent.image +'"><p>In This Event:<br/><a href="'+viewEvent.relatedlink +'">Apple</a></p></div><div class="right-panel"><h4>On this Day:</h4><p>'+viewEvent.summary+'</p><p><button name="share-btn" class="share-button">Share</button></p></div></div>';
+  
+  this.previousIndex = viewEvent.previousIndex;
+  this.nextIndex = viewEvent.nextIndex;
+  
+};
